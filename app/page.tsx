fb6e15f27504f8c9783ddde54fc77ac5595cfb5d
zua <farhan.javed006@gmail.com>
2025-05-25 16:37:18 +0530

    smooth-custom-js-no-motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 import styles from './page.module.scss';
-import { motion } from 'motion/react';
-import useMousePosition from './utils/useMousePosition';
 import useSmoothMousePosition from './utils/useSmoothMousePosition';
 
 export default function Home() {
@@ -10,8 +8,8 @@ export default function Home() {
 
   return (
     <main className={styles.hero}>
-      {/* Grid that gets revealed by cursor */}
-      <motion.div
+
+      <div
         className={styles.grid}
         style={{
           maskImage: `radial-gradient(circle at ${x}px ${y}px, 
@@ -29,15 +27,8 @@ export default function Home() {
           transparent 200px
         )`,
         }}
-        transition={{
-          type: "spring",
-          damping: 30,
-          stiffness: 200,
-          mass: 0.8
-        }}
       />
 
-      {/* Text that's always visible */}
       <div className={styles.heroText}>
         <h1>less is less.</h1>
         <h2>MORE IS MORE.</h2>

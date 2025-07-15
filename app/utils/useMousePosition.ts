da93ddc9fe7ecb295d1b12ba43ac903f5c7e083b
frhanjav <farhan.javed006@gmail.com>
2025-07-15 19:28:24 +0530

    centering-mouse-position

--- app/utils/useMousePosition.ts
+++ app/utils/useMousePosition.ts
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
 
 export default function useMousePosition() {
+  const [mousePosition, setMousePosition] = useState(() => ({
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+  }));
 
-    const [mousePosition, setMousePosition] = useState({ x:0, y:0 });
+  const updateMousePosition = (e: MouseEvent) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
 
-    const updateMousePosition = (e : MouseEvent) => {
-        setMousePosition({ x:e.clientX, y:e.clientY });
-    };
+  useEffect(() => {
+    window.addEventListener('mousemove', updateMousePosition);
+    return () => window.removeEventListener('mousemove', updateMousePosition);
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener('mousemove', updateMousePosition);
-
-        return () => {
-            window.removeEventListener('mousemove', updateMousePosition);
-        };
-    }, []);
-
-    return mousePosition;
+  return mousePosition;
 }
